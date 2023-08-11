e8713d733d81c0d503f57342f9bf1bbec535ad05
IceLordUlmo <torgall22@gmail.com>
2023-08-11 10:15:54 -0500

    Model setup

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,11 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .attack import Attack
+from .encounter import Encounter
+from .fight import Fight
+from .inventoryitem import InventoryItem
+from .item import Item
+from .location import Location
+from .monster import Monster
+from .userattack import UserAttack
