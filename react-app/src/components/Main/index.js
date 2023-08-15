47c088a1842189316e05c74cb45febd5a9e1a016
IceLordUlmo <torgall22@gmail.com>
2023-08-15 03:23:46 -0500

    log in and out

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from "../../store/session"
+
 
 const Main = () => {
     const user = useSelector((state) => state.session.user)
+    const dispatch = useDispatch();
+
+    function LogOut(e) {
+        e.preventDefault();
+        dispatch(logout());
+    }
 
     return (
         <div>
             User : {user.username}
             Health : {user.current_hp} / {user.max_hp}
+            <button onClick={LogOut}>Log Out</button>
         </div>
     )
 }
