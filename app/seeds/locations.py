8915ee3ed2958cdc64ae157156ba25ef4351c9d6
IceLordUlmo <torgall22@gmail.com>
2023-08-21 01:40:46 -0500

    map styling

--- app/seeds/locations.py
+++ app/seeds/locations.py
@@ -27,10 +27,45 @@ def seed_locations():
         required_level = 999,
         image_url = "seedLocation4.jpg"
         )
+    demo5 = Location(
+        name = 'The Forbidden Archives 2',
+        description = "If you can get in here, I'm not even mad.",
+        required_level = 999,
+        image_url = "seedLocation5.jpg"
+        )
+    demo6 = Location(
+        name = 'The Forbidden Archives 3',
+        description = "If you can get in here, I'm not even mad.",
+        required_level = 999,
+        image_url = "seedLocation6.jpg"
+        )
+    demo7 = Location(
+        name = 'The Less Forbidden Archives',
+        description = "Not as secret as the others...",
+        required_level = 3,
+        image_url = "seedLocation7.jpg"
+        )
+    demo8 = Location(
+        name = 'The Forbidden Archives',
+        description = "If you can get in here, I'm not even mad.",
+        required_level = 999,
+        image_url = "seedLocation8.jpg"
+        )
+    demo9 = Location(
+        name = 'The Forbidden Archives',
+        description = "If you can get in here, I'm not even mad.",
+        required_level = 999,
+        image_url = "seedLocation9.jpg"
+        )
     db.session.add(demo1)
     db.session.add(demo2)
     db.session.add(demo3)
     db.session.add(demo4)
+    db.session.add(demo5)
+    db.session.add(demo6)
+    db.session.add(demo7)
+    db.session.add(demo8)
+    db.session.add(demo9)
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
--- react-app/src/components/Maps/MapLocation.js
+++ react-app/src/components/Maps/MapLocation.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
 import './MapLocation.css'
 
-const MapLocation = ({ location }) => {
+const MapLocation = ({ location, level }) => {
     return (
         <a>
-            <p>Location: <Link to={`/maps/${location.id}`}>{location.name}</Link></p>
-            <p>Description: {location.description}</p>
+            <div id='location-for-grid'>{level >= location.required_level ?
+                <div id='location-name-for-grid'><Link to={`/maps/${location.id}`}><h2>{location.name}</h2></Link></div> :
+                <div id='location-name-for-grid' class='inaccessible'><h2>{location.name}</h2> <div>Requires level {location.required_level} to enter.</div></div>}
+                <div>{location.description}</div>
+            </div>
         </a>
     )
 
