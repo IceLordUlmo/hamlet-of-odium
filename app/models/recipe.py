2b19aacbc3005a0c64417cbaa6291651f3fbc04f
IceLordUlmo <torgall22@gmail.com>
2023-08-25 18:09:22 -0500

    recipe and ingredient

---
+++ app/models/recipe.py
@@ -0,0 +1,22 @@
+from .db import db, add_prefix_for_prod, environment, SCHEMA
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    image_url = db.Column(db.String(255), nullable=False)
+    slot = db.Column(db.String(50))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "image_url": self.image_url,
+            "slot": self.slot
+        }
